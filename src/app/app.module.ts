48e1112b5a4439c9cc80b35b7dfb2602791893c0
Jerry Satpathy <rutu.satpathy44@gmail.com>
2019-01-30 14:48:40 +0530

    App Inited

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { WeatherComponent } from './weather/weather.component';
+import { ForecastComponent } from './forecast/forecast.component';
+import { WindComponent } from './wind/wind.component';
+import { SearchbarComponent } from './searchbar/searchbar.component';
+import { WeatherParentComponent } from './weather-parent/weather-parent.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WeatherComponent,
+    ForecastComponent,
+    WindComponent,
+    SearchbarComponent,
+    WeatherParentComponent
   ],
   imports: [
     BrowserModule
